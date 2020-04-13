b7f6c767154dee21e8871b76a19d4790c43c9df0
Arszilla <arszilla@outlook.com>
2020-04-13 17:11:59 +0300

    Add disclaimer about member IDs

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+# Add all the users IDs that will interact with the app
+# In this case below, 3 users will be able to interact with the app
+# If a single person was to interact with the bot, replace "id_1" with person's ID and remove "id_2" and "id_3"
 member_id = ["id_1", "id_2", "id_3"]
 
-oauth_access_token = "xoxp-"
 bot_user_oauth_token = "xoxb-"
 
 channel_prefix = "sierra-hotel-"
